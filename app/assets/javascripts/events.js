9c097ba60b966905186e275274bbac6899538789
Raji Ayinla <rajiayinla858@gmail.com>
2017-02-17 16:37:25 -0800

    add events for layout-hero

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,12 +1,40 @@
 $(document).ready(function(){
 	$(window).scroll(function(){
 	    if($(this).scrollTop() > 1){
-	      $('div.layout-header').addClass('sticky');  
+	      $('div.layout-header').addClass('sticky');
+	    
 	    }
 	    else{
 	      $('div.layout-header').removeClass('sticky');
 	    }
-	  });	
+	  });
+
+	  $('.back-end').on('mouseover', function(){
+	   $('.front-end').siblings('div.line_left').addClass('line_active')
+	    $('.front-end').siblings('div.line_right').addClass('line_thin')
+	  })
+	  
+	 $('.back-end').on('mouseout', function(){
+	   $('.front-end').siblings('div.line_left').removeClass('line_active')
+	   $('.front-end').siblings('div.line_right').removeClass('line_thin')
+	  })
+
+	$('.front-end').on('mouseover', function(){
+	   $('.front-end').siblings('div.line_right').addClass('line_active')
+	   $('.back-end').siblings('div.line_left').addClass('line_thin')
+	  })
+	  
+	 $('.front-end').on('mouseout', function(){
+	   $('.front-end').siblings('div.line_right').removeClass('line_active')
+	    $('.back-end').siblings('div.line_left').removeClass('line_thin')
+	  })
+
+	$('.nav li').on("click", function(e){
+	  that = $(e.target)
+	   that.addClass('.nav-active')
+	  $('body').animate({scrollTop:500},'slow' ) 
+	   return false;
+	})
 });
   
   
